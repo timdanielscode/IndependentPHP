a93e77e57b9fa9c44ac5ac186f01814aae666604
timdanielscode <timdanielscode@hotmail.com>
2023-04-18 14:56:47 +0100

    Removed code from validation Rules class

--- validation/Rules.php
+++ validation/Rules.php
@@ -7,19 +7,16 @@
 namespace validation;
 
 use core\validation\Validate;
-use app\controllers\Controller;
-use core\Session;
 
 class Rules {
 
     public $errors;
 
     /**
-     * You can add the validation rule methods right here
-     * 
      * https://indy-php.com/docs/validation
      * 
-     * https://indy-php.com/docs/
+     * You can add the validation rule methods right here
+     * 
      * Chain input method on instance variable and add html input name as argument
      * Chain as method to input and add alias as argument
      * Chain rules method to as method and add array of validation rules as argument
@@ -32,23 +29,14 @@ class Rules {
 
     /**
      * Validating validation rules
-     * On fail, returning view with extracted validation error rules and if exists request data
      * 
-     * @return mixed bool | void
+     * @return bool
      */
     public function validated($request = null) {
 
-        if(empty($this->errors) ) { return true; } else {
+        if(empty($this->errors) ) {
 
-          if(!empty($request) && $request !== null) {
-            
-              $data['data'] = $request; 
-          }
-
-          $controller = new Controller();
-          $data['rules'] = $this->errors;
-
-          return $controller->view(Session::get('view'), $data) . exit();
+            return true;
         }
     }
 }
