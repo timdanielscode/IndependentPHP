8276e155f06353219f578c944916ff3712e60392
timdanielscode <timdanielscode@hotmail.com>
2023-07-24 13:05:23 +0000

    Update Csrf class and cleaned up code

--- core/Csrf.php
+++ core/Csrf.php
@@ -7,51 +7,129 @@
 namespace core;
 
 use core\Session;
-use core\http\Request;
 
 class Csrf {
 
     /**
-     * Generating csrf token if not exists
-     * can be used to add a token and get the token 
-     * 
-     * @param string $arg get|add
-     * @return string token csrf
+     * @param string $arg get | add
+     * @return mixed string token | void
      */
     public static function token($arg) {
 
-        if(!Session::exists('Csrf_token')) {
-            Session::set('Csrf_token', bin2hex(random_bytes(32)));
-        }
+        $token = self::generate();
+        return self::getOrAddToken($arg, $token);
+    }
 
-        $token = hash_hmac('sha256', 'hash me please!', Session::get('Csrf_token'));
-        
-        if($arg == 'get') {
-            return $token;
-        } else if($arg == 'add') {
-            echo $token;
+    /**
+     * Generates token
+     * 
+     * @return string session token
+     */
+    private static function generate() {
+
+        if(!Session::exists('csrf')) {
+
+            Session::set('csrf', bin2hex(random_bytes(32)));
+        } 
+
+        return Session::get('csrf');
+    }
+
+    /**
+     * @param string $type add | get
+     * @param string token token
+     * @return mixed string token | void
+     */
+    private static function getOrAddToken($type, $token) {
+
+        if($type === 'get') {
+
+            return self::getToken($token);
+        } else if($type === 'add') {
+
+            return self::addToken($token);
+        } else {
+            return;
         }
     }
 
     /**
-     * Validating the token
-     * If validation fails, setting csrf session and 
-     * redirecting back to current request uri global value
+     * @param string $token token
+     * @return string token
+     */
+    private static function getToken($token) {
+
+        return $token;
+    }
+
+    /**
+     * @param string $token token
+     * @return void
+     */
+    private static function addToken($token) {
+
+        echo $token;
+    }
+
+    /**
+     * Validating token
      * 
-     * @param string $token value
-     * @param string $postToken value
-     * @return bool true 
+     * @param string $token token
+     * @param string $postToken post token
+     * @return bool
      */ 
     public static function validate($token, $postToken) {
 
-        if(hash_equals($token, $postToken) === false) {
+        if($token !== $postToken) {
 
-            $request = new Request();
-            Session::set('csrf', 'Cross site request forgery!');
-            redirect($request->getUri()) . exit();
+            self::setMessage();
+
+            return false;
         } else {
 
+            self::deleteToken();
+            self::deleteMessage();
+
             return true;
+        }
+    }
+
+    /**
+     * Setting session token message
+     * 
+     * @return void
+     */     
+    private static function setMessage() {
+
+        if(!Session::exists('csrf_message')) {
+
+            Session::set('csrf_message', 'Cross site request forgery!');
+        }
+    }
+
+    /**
+     * Deleting session token message
+     * 
+     * @return void
+     */     
+    private static function deleteMessage() {
+
+        if(Session::exists('csrf_message')) {
+
+            Session::delete('csrf_message');
+        }
+    }
+
+    /**
+     * Deleting token
+     * 
+     * @return void
+     */     
+    private static function deleteToken() {
+
+        if(Session::exists('csrf')) {
+
+            Session::delete('csrf');
         }
     }
 }
